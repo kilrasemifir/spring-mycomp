27a10488054dfb85376f3c5a69dc9cc480593e41
Killian Raoux <killian@semifir.com>
2022-12-01 09:59:04 +0100

    first commit

---
+++ src/main/java/fr/kira/formation/spring/competences/mycomp/MycompApplication.java
@@ -0,0 +1,13 @@
+package fr.kira.formation.spring.competences.mycomp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycompApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/kira/formation/spring/competences/mycomp/MycompApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.kira.formation.spring.competences.mycomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
