4f9e06f505b6eaefe353a12eb069088d8e74502c
Killian Raoux <killian@semifir.com>
2022-12-01 12:10:52 +0100

    chore: WIP ajout des personnes dans les equipes

--- src/main/java/fr/kira/formation/spring/competences/mycomp/equipes/Equipe.java
+++ src/main/java/fr/kira/formation/spring/competences/mycomp/equipes/Equipe.java
@@ -1,8 +1,12 @@
 package fr.kira.formation.spring.competences.mycomp.equipes;
 
+import fr.kira.formation.spring.competences.mycomp.personnes.Personne;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Document
@@ -10,4 +14,5 @@ public class Equipe {
     @Id
     private String id;
     private String nom;
+    private List<Personne> membres = new ArrayList<>();
 }
